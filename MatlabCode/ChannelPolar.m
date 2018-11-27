7c116f29b5d91d9dbab00ef24e8b27bf140ad99d
LimoJ <CatfoodJiang@outlook.com>
2018-11-27 14:46:23 +0800

    add PolarDecode function and its dependent functions;add ChannelPolar function and UVectorConstruct and Deconstruct function;finish polar floatpoint simulation

---
+++ MatlabCode/ChannelPolar.m
@@ -0,0 +1,18 @@
+function [Ua,Uac] = ChannelPolar(N,K)
+%BEC-Z(W) ChannelPolar
+% Z(W) init value is 0.5
+
+y=ZParameter(0.5,N);
+[i,j]=sort(y);
+freeze_bit_set=j(K+1:N);
+free_bit_set=j(1:K);
+Uac=zeros(1,N);
+Ua=zeros(1,N);
+for i=freeze_bit_set(1:N-K)
+    Uac(i)=1;
+end
+for i=free_bit_set(1:K)
+    Ua(i)=1;
+end
+end
+
