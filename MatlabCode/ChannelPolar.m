6de7134aa1a78a59b211aafb14d87b17714c7d9e
LimoJ <CatfoodJiang@outlook.com>
2018-11-29 19:33:18 +0800

    add noiseVar para to init Z(W)

--- MatlabCode/ChannelPolar.m
+++ MatlabCode/ChannelPolar.m
@@ -1,8 +1,8 @@
-function [Ua,Uac] = ChannelPolar(N,K)
+function [Ua,Uac] = ChannelPolar(N,K,noiseVar)
 %BEC-Z(W) ChannelPolar
 % Z(W) init value is 0.5
 
-y=ZParameter(0.5,N);
+y=ZParameter(exp(-1/(2*noiseVar)),N);
 [i,j]=sort(y);
 freeze_bit_set=j(K+1:N);
 free_bit_set=j(1:K);
